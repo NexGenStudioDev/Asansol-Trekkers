8cd65d632fa5887380241ab375888dd546046d1e
Anjali Tomar <anjali.theaj3366@gmail.com>
2025-11-06 12:54:17 +0530

    feat: Enhance App structure with new sections and components
    - Updated App.jsx to include new sections: Team, Testimonials, Contact, and Footer.
    - Added ContactGallery component for displaying images in the Contact section.
    - Created Footer component with contact information and social media links.
    - Modified Nav component for improved navigation and updated button text.
    - Introduced TestimonialCard component for displaying user testimonials.
    - Developed Contact section with a form for user inquiries.
    - Implemented Team section with a carousel of team members.
    - Replaced old Testimonial section with a new Testimonials section featuring animations.
    - Adjusted UpcomingEvent section for better layout and responsiveness.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import PopularDestinations from './Section/PopularDestinations';
-import Hero from './Section/Hero';
-import Nav from './Components/Nav';
-import About from './Section/About';
-import Gallery from './Section/Gallery';
-import TourAndTraver from './Section/TourAndTraver';
-import TopDestinations from './Section/TopDestinations';
-import UpcomingEvent from './Section/UpcomingEvent';
+import React from "react";
+import PopularDestinations from "./Section/PopularDestinations";
+import Hero from "./Section/Hero";
+import Nav from "./Components/Nav";
+import About from "./Section/About";
+import Gallery from "./Section/Gallery";
+import TourAndTraver from "./Section/TourAndTraver";
+import TopDestinations from "./Section/TopDestinations";
+import UpcomingEvent from "./Section/UpcomingEvent";
+import Footer from "./Components/Footer";
+import Team from "./Section/Team";
+import Testimonials from "./Section/Testimonials";
+import Contact from "./Section/Contact";
 
 const App = () => {
   return (
     <>
-    <Nav />
-    <Hero />
-    <About />
-    <PopularDestinations />
-    <TopDestinations />
-    {/* <TourAndTraver /> */}
-    <Gallery />
-    <UpcomingEvent />
-    <div className="h-screen w-screen bg-red-400"></div>
+      <Nav />
+      <Hero />
+      <About />
+      <PopularDestinations />
+      <TopDestinations />
+      {/* <TourAndTraver /> */}
+      <Gallery />
+      <Team />
+      <UpcomingEvent />
+      <Testimonials />
+      <Contact />
+      <Footer />
+
+      {/* <div className="h-screen w-screen bg-red-400"></div> */}
     </>
   );
 };
